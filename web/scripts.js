9751d2bca6d65e7f332fbf21de69911a7f42262e
luis96920 <lucasm3938@gmail.com>
2023-03-27 00:16:30 -0400

    initial

--- web/scripts.js
+++ web/scripts.js
@@ -4,34 +4,21 @@ $(document).ready(function () {
     const imageInput = $("#image-input");
     const originalImage = $("#original-image");
     const processedImage = $("#processed-image");
-    const processImageBtn = $("#process-image");
 
     uploadForm.on("submit", function (event) {
         event.preventDefault();
         const formData = new FormData();
         formData.append("image", imageInput[0].files[0]);
 
-        axios.post("/upload", formData, {
+        axios.post("/upload_and_process", formData, {
             headers: {
                 "Content-Type": "multipart/form-data",
             },
         })
         .then(response => {
-            originalImage.attr("src", response.data.url);
+            originalImage.attr("src", response.data.original_url);
             originalImage.show();
-            processImageBtn.show();
-        })
-        .catch(error => {
-            console.error(error);
-        });
-    });
-
-    processImageBtn.on("click", function () {
-        const originalImageUrl = originalImage.attr("src");
-        axios.post("/process", {url: originalImageUrl})
-        .then(response => {
-            const processedImageUrl = response.data.url;
-            processedImage.attr("src", processedImageUrl);
+            processedImage.attr("src", response.data.processed_url);
             processedImage.show();
         })
         .catch(error => {
